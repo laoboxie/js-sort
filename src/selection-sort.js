93e15f65341abc9e0e43f42a5fe93b32be94c1e3
谢远博 <xieyuanbo@21fid.cn>
2020-07-13 17:53:31 +0800

    feat 增加原理描述

--- src/selection-sort.js
+++ src/selection-sort.js
@@ -2,6 +2,7 @@
   时间复杂度  O(n^2)
   空间复杂度  O(1)
   是否稳定    否
+  原理：每次选择最小的元素，放到已排序的队列尾部，在未排序的队列中重复此操作
 */
 
 const until = require("./until");
@@ -22,6 +23,7 @@ function selectionSort(arr, compare) {
     }
     swap(arr, i, frontIdx);
   }
+  return arr;
 }
 
 module.exports = selectionSort;
